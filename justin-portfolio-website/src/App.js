3389f2f356f46dd3a2faa6194970a43dfa632b4c
dga48 <darrickgunawan@gmail.com>
2024-02-02 02:22:02 -0800

    updated stuff
    thanks justin

--- justin-portfolio-website/src/App.js
+++ justin-portfolio-website/src/App.js
@@ -17,7 +17,8 @@ function App() {
 
         <div className="text-container">
           <h1>{name}</h1>
-          <p>Nico nico niii anata no heart ni nico nico niiii egao todokeru yazawa nico nico nico ni te oboeteru love nico! We're no strangers to love, you know the rules and so do I.  A full committments what im thinking of, you wouldn't get this from any other guy. I just wanna tell you how im feeling, gotta make you, understand. Never gonna give you up, never gonna let you down, never gonna turn around and hurt you.</p>
+          <p>Biomedical Engineering student</p>
+          
         </div>
       </div>
       
@@ -26,7 +27,7 @@ function App() {
         <Card title='Test Title' imageUrl='https://thumbs.dreamstime.com/b/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg' imageAlt='Test Image' body='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
         <Card title='Test Title' imageUrl='https://thumbs.dreamstime.com/b/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg' imageAlt='Test Image' body='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
         <Card title='Test Title' imageUrl='https://thumbs.dreamstime.com/b/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg' imageAlt='Test Image' body='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />
-        <Card title='Test Title' imageUrl='https://thumbs.dreamstime.com/b/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg' imageAlt='Test Image' body='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />     
+        <Card title='Test Title' imageUrl='https://thumbs.dreamstime.com/b/world-map-technological-background-best-internet-concept-global-business-elements-image-furnished-bright-lines-66529240.jpg' imageAlt='Test Image' body='sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' />   
       </div>
     </div>
   );
--- justin-portfolio-website/src/components/TopBar/TopBar.js
+++ justin-portfolio-website/src/components/TopBar/TopBar.js
@@ -7,7 +7,7 @@ function TopBar() {
             <nav>
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">Portfolio</a></li>
+                    <li><a href="#">Resume</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
